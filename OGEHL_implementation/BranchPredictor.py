e6cc4236b28c32c236e41380cb751bbcf3ea36b7
alkalizk <157429643+alkalizk@users.noreply.github.com>
2026-04-11 18:02:59 -0400

    Update BranchPredictor.py

--- OGEHL_implementation/BranchPredictor.py
+++ OGEHL_implementation/BranchPredictor.py
@@ -5,22 +5,22 @@ class OGEHLBP(ConditionalPredictor):
     cxx_class = 'gem5::branch_prediction::OGEHLBP'
     cxx_header = "cpu/pred/ogehl.hh"
 
-    num_tables = Param.Unsigned(8, "Number of O-GEHL tables")
-    table_size = Param.Unsigned(2048, "Entries per table")
-    counter_bits = Param.Unsigned(4, "Counter width")
+    num_tables = Param.Unsigned(8, "Number of O-GEHL tables") # number of tables (table is array of counters)
+    table_size = Param.Unsigned(2048, "Entries per table")    # size of tables
+    counter_bits = Param.Unsigned(4, "Counter width")         # size of a single saturating counter
 
-    theta = Param.Unsigned(8, "Initial update threshold")
-    max_theta = Param.Unsigned(31, "Maximum dynamic threshold")
-    tc_bits = Param.Unsigned(7, "Threshold counter bits")
+    theta = Param.Unsigned(8, "Initial update threshold")     # threshold
+    max_theta = Param.Unsigned(31, "Maximum dynamic threshold") # set a limit for theta
+    tc_bits = Param.Unsigned(7, "Threshold counter bits")       # theta only update when tc_bit reachs maximum or minimum
 
-    global_history_bits = Param.Unsigned(200, "Global history length")
+    global_history_bits = Param.Unsigned(200, "Global history length") 
 
     short_history_lengths = VectorParam.Unsigned(
         [0, 3, 5, 8, 12, 19, 31, 49],
         "Short history lengths"
     )
 
-    long_history_lengths = VectorParam.Unsigned(
+    long_history_lengths = VectorParam.Unsigned( #only T2, T4, T6 have longer version, same with the paper 
         [0, 0, 79, 0, 125, 0, 200, 0],
         "Long history lengths for dynamic tables"
     )
